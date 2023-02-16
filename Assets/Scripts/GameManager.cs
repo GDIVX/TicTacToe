4f4a8b7136ae428ab3792d7b236378cba5b3d8e7
GDIVX <yarin107@gmail.com>
2023-02-16 17:14:15 +0200

    Add project files.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //singlton
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
